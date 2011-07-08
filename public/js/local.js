9b6574e7a7e9beb01e2e54656df7ac1e793eadb5
jettro <jettro@gridshore.nl>
2011-07-08 09:31:39 +0200

    changed the handling of receiving events through nowjs on the client

--- public/js/local.js
+++ public/js/local.js
@@ -6,10 +6,22 @@ $(document).ready(function() {
     now.receiveContact = function(data) {
         var message = JSON.parse(data);
         var toWrite;
-        if (message.type == 'removed') {
-            toWrite = "Contact " +  message.type;
-        } else {
-            toWrite = message.content.name + " " + message.type;
+        switch(message.type) {
+            case 'contact-removed':
+                toWrite = "Contact removed";
+                break;
+            case 'contact-created':
+                toWrite = "Contact with name " + message.content.name + " created";
+                break;
+            case 'contact-changed':
+                toWrite = "Contact changed name to " + message.content.name;
+                break;
+            case 'address-created':
+                toWrite = "Added address of type " + message.content.addressType + " to contact " + message.content.name;
+                break;
+            case 'address-removed':
+                toWrite = "Removed address of type " + message.content.addressType + " to contact " + message.content.contact.name;
+                break;
         }
         $("#messages").prepend("<div>" + toWrite + " </div>");
     };
